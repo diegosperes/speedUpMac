e0112e7761d92dac3d5cc3e8743ef5a04ec792e6
Diego Péres <diego.peres@corp.globo.com>
2017-12-07 21:13:27 -0200

    Disable swap memory and enable data compress in memory ram

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 # Remove some caches
+
 rm -rf ~/Library/Caches/*
+
+# By default mac will enable the swap memory and does not compress any data to optimize the use of memory ram
+# So we will disable the swap memory to only use the memory ram and enabled the data compress
+# This example will set nvram mode to default: sudo nvram boot-args="vm_compressor=4
+# https://gist.github.com/ayltai/eaef3ba7577f980beafecf827d645710#file-vm_pageout-h
+
+echo "Current nvram mode: $(sysctl -a vm.compressor_mode)"
+sudo nvram boot-args="vm_compressor=2"
